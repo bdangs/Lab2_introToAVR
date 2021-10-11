4be0d144694d8fd6398b619aa9f9c468d41d2338
Bryan Dang <bddangs@gmail.com>
2021-10-10 22:02:39 -0700

    Completed part 1

--- turnin/bdang014_lab2_part1.c
+++ turnin/bdang014_lab2_part1.c
@@ -1,7 +1,7 @@
 /*	Author: Bryan Dang
  *  Partner(s) Name: 
  *	Lab Section: 021
- *	Assignment: Lab #2  Exercise #0
+ *	Assignment: Lab #2  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
